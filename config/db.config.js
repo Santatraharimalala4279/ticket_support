14ddcb95110ba0129c952cf65d07686d50b945d3
=Santatra RAKOTOHARIMALALA <=santatrarakotoharimalala@gmail.com>
2022-11-13 21:26:37 +0300

    Configuration de la base de données depuis cloud

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,13 @@
 let Sequelize = require("sequelize");
-let db = new Sequelize("tickets_support", "root", "root", {
-  host: "localhost",
-  dialect: "mysql",
-  logging: false, //passer a true pour voir les différentes requêtes effectuées par l'ORM
-});
+let db = new Sequelize(
+  "santatra_tickets_support",
+  "santatra",
+  "ticket@support*.",
+  {
+    host: "mysql-santatra.alwaysdata.net",
+    dialect: "mysql",
+    logging: false, //passer a true pour voir les différentes requêtes effectuées par l'ORM
+  }
+);
 //on exporte pour utiliser notre connexion depuis les autre fichiers.
 module.exports = db;
