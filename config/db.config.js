f8c0b22a6771f4f37d2a9e7182001d3e79e84830
=Santatra RAKOTOHARIMALALA <=santatrarakotoharimalala@gmail.com>
2022-11-14 11:13:42 +0300

    Reconfiguration de package.json

--- config/db.config.js
+++ config/db.config.js
@@ -1,13 +1,10 @@
+require("dotenv").config();
 let Sequelize = require("sequelize");
-let db = new Sequelize(
-  "santatra_tickets_support",
-  "santatra",
-  "ticket@support*.",
-  {
-    host: "mysql-santatra.alwaysdata.net",
-    dialect: "mysql",
-    logging: false, //passer a true pour voir les différentes requêtes effectuées par l'ORM
-  }
-);
+const env = process.env;
+let db = new Sequelize(env.DB_DATABASE, env.DB_USER, env.DB_PASSWORD, {
+  host: env.DB_HOST,
+  dialect: "mysql",
+  logging: false, //passer a true pour voir les différentes requêtes effectuées par l'ORM
+});
 //on exporte pour utiliser notre connexion depuis les autre fichiers.
 module.exports = db;
