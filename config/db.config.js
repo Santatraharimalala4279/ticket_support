8b4ecd062c625c0f5f9acc9c95b97ddd9da9f481
=Santatra RAKOTOHARIMALALA <=santatrarakotoharimalala@gmail.com>
2022-11-14 15:44:46 +0300

    Rectification de la mot de passe del base de données dans .env file

--- config/db.config.js
+++ config/db.config.js
@@ -7,4 +7,11 @@ let db = new Sequelize(env.DB_DATABASE, env.DB_USER, env.DB_PASSWORD, {
   logging: false, //passer a true pour voir les différentes requêtes effectuées par l'ORM
 });
 //on exporte pour utiliser notre connexion depuis les autre fichiers.
+db.authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.log("Unable to connect to the database:", err);
+  });
 module.exports = db;
